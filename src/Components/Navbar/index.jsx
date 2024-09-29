1031be70a1ac3fd35b44d51afed8d4fb899bc1bb
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-09-29 00:24:42 -0500

    subiendo cambios en los items

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,70 +1,70 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
-    return (
-        <nav className='flex justify-between items-center fixed z-10'>
-            <ul className='flex items-center'>
-                <li>
-                    <NavLink to='/'>
-                        Shopi
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/'>
-                        All
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/clothes'>
-                        Clothes
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/electronics'>
-                        Electronics
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/furnitures'>
-                        Furnitures
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/toys'>
-                        Toys
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/others'>
-                        Others
-                    </NavLink>
-                </li>
-            </ul>
-            <ul>
-                <li>
-                    alexa@platzi.com
-                </li>
-                <li>
-                    <NavLink to='/my-orders'>
-                        My Orders
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/my-account'>
-                        My Account
-                    </NavLink>
-                </li>
-                <li>
-                    <NavLink to='/sign-in'>
-                        Sign In
-                    </NavLink>
-                </li>
-                <li>
-                    🛒 0
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className='flex justify-between items-center fixed z-10'>
+        <ul className='flex items-center'>
+            <li>
+                <NavLink to='/'>
+                    Shopi
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/'>
+                    All
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/clothes'>
+                    Clothes
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/electronics'>
+                    Electronics
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/furnitures'>
+                    Furnitures
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/toys'>
+                    Toys
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/others'>
+                    Others
+                </NavLink>
+            </li>
+        </ul>
+        <ul className='flex items-center'>
+            <li>
+                alexa@platzi.com
+            </li>
+            <li>
+                <NavLink to='/my-orders'>
+                    My Orders
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/my-account'>
+                    My Account
+                </NavLink>
+            </li>
+            <li>
+                <NavLink to='/sign-in'>
+                    Sign In
+                </NavLink>
+            </li>
+            <li>
+                🛒 0
+            </li>
+        </ul>
+    </nav>
+  )
 }
 
-export default Navbar;
+export default Navbar
