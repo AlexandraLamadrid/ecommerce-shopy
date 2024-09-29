8de5e762225936961694618f56626f2da0f24fab
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-09-29 01:07:49 -0500

    subiendo las correciones en el navbar, colocando underline y cambiando estilo del correo

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,70 +1,97 @@
-import { NavLink } from 'react-router-dom'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
-  return (
-    <nav className='flex justify-between items-center fixed z-10'>
-        <ul className='flex items-center'>
-            <li>
-                <NavLink to='/'>
-                    Shopi
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/'>
-                    All
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/clothes'>
-                    Clothes
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/electronics'>
-                    Electronics
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/furnitures'>
-                    Furnitures
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/toys'>
-                    Toys
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/others'>
-                    Others
-                </NavLink>
-            </li>
-        </ul>
-        <ul className='flex items-center'>
-            <li>
-                alexa@platzi.com
-            </li>
-            <li>
-                <NavLink to='/my-orders'>
-                    My Orders
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/my-account'>
-                    My Account
-                </NavLink>
-            </li>
-            <li>
-                <NavLink to='/sign-in'>
-                    Sign In
-                </NavLink>
-            </li>
-            <li>
-                🛒 0
-            </li>
-        </ul>
-    </nav>
-  )
-}
+  const activeStyle = 'underline underline-offset-4';
 
-export default Navbar
+  return (
+    <nav className="flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light">
+      <ul className="flex items-center gap-3">
+        <li className="font font-semibold text-lg">
+          <NavLink to="/">
+            Shopi
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            All
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/clothes"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Clothes
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/electronics"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Electronics
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/furnitures"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Furnitures
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/toys"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Toys
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/others"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Others
+          </NavLink>
+        </li>
+      </ul>
+      <ul className="flex items-center gap-3">
+        <li className='text-black/60'>
+            alexa@platzi.com
+        </li>
+        <li>
+          <NavLink
+            to="/my-orders"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            My Orders
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/my-account"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            My Account
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="/sign-in"
+            className={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            Sign In
+          </NavLink>
+        </li>
+        <li>🛒 0</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
