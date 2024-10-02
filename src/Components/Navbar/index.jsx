9b972dfeb4987dc3ad7f6a7a87bc93b7ce64cf25
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-10-02 11:16:24 -0500

    subiendo cambios en el carrito para que salga el numero de items

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import { NavLink } from "react-router-dom";
+import { ShoppingCartContext } from "../../Context";
 
 const Navbar = () => {
+  const context = useContext(ShoppingCartContext)
   const activeStyle = 'underline underline-offset-4';
 
   return (
@@ -88,7 +91,9 @@ const Navbar = () => {
             Sign In
           </NavLink>
         </li>
-        <li>🛒 0</li>
+        <li>
+          🛒 {context.count}
+        </li>
       </ul>
     </nav>
   );
