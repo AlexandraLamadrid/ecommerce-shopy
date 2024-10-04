72dc6304d4cf3cdbe99bdfc0fac4029e04670635
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-10-03 21:55:12 -0500

    subiendo cambios para que la ventana de productDetail cierre

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,10 +4,17 @@ import { createContext, useState } from 'react'
 export const ShoppingCartContext = createContext()
 
 export const ShoppingCartProvider = ({children}) => {
+    // Shopping Cart - Increment quantity
     const [count, setCount] = useState(0)
+
+    // Product Detail - Open/Close
     const [isProductDetailOpen, setIsProductDetailOpen] = useState(false)
     const openProductDetail = () => setIsProductDetailOpen(true)
     const closeProductDetail = () => setIsProductDetailOpen(false)
+
+    // Product Detail - Show product
+    const [productToShow, setProductToShow] = useState({})
+
 
     return (
         <ShoppingCartContext.Provider value={{
@@ -15,7 +22,9 @@ export const ShoppingCartProvider = ({children}) => {
             setCount,
             openProductDetail,
             closeProductDetail,
-            isProductDetailOpen
+            isProductDetailOpen,
+            productToShow,
+            setProductToShow
         }}>
             {children}
         </ShoppingCartContext.Provider>
