6d9bdcc9f55e884b2486ff6cb5b0a95920635a33
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-10-05 21:38:56 -0500

    subiendo cambios en la api que no quiere funcionar

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -3,36 +3,29 @@ import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 import ProductDetail from '../../Components/ProductDetail'
 
-
 function Home() {
-  const [items, setItems] = useState([])
+  const [items, setItems] = useState(null)
 
   useEffect(() => {
     fetch('https://api.escuelajs.co/api/v1/products')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
+      .then(response => response.json())
       .then(data => setItems(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }, []);
-  
+  }, [])
 
-    return (
-        <Layout>
-          Home
-          <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 w-full max-w-screen-lg'>
-            {
-              items?.map(item => (
-                <Card key={item.id} data={item} />
-              ))
-            }
-          </div>
-          <ProductDetail />
-        </Layout>
-    )
-  }
-  
-  export default Home
+  return (
+    <Layout>
+      Home
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (
+            <Card key={item.id} data={item} />
+          ))
+        }
+      </div>
+      <ProductDetail />
+    </Layout>
+  )
+}
+
+
+export default Home
