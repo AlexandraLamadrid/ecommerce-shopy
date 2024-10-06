25f158f0a6fcb662ad32728cc233f4b09eb4a979
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-10-06 02:30:15 -0500

    subiendo ultimos cambios en el filtrado

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -8,93 +8,108 @@ const Navbar = () => {
   const activeStyle = 'underline underline-offset-4';
 
   return (
-    <nav className="flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light">
-      <ul className="flex items-center gap-3">
-        <li className="font font-semibold text-lg">
-          <NavLink to="/">
+    <nav className='flex justify-between items-center fixed z-10 top-0 w-full py-5 px-8 text-sm font-light'>
+      <ul className='flex items-center gap-3'>
+        <li className='font font-semibold text-lg'>
+          <NavLink to='/'>
             Shopi
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/'
+            onClick={() => context.setSearchByCategory()}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             All
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/clothes"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/clothes'
+            onClick={() => context.setSearchByCategory('clothes')}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Clothes
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/electronics"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/electronics'
+            onClick={() => context.setSearchByCategory('electronics')}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Electronics
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/furnitures"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/furnitures'
+            onClick={() => context.setSearchByCategory('furnitures')}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Furnitures
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/toys"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/toys'
+            onClick={() => context.setSearchByCategory('toys')}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Toys
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/others"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/others'
+            onClick={() => context.setSearchByCategory('others')}
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Others
           </NavLink>
         </li>
       </ul>
-      <ul className="flex items-center gap-3">
+      <ul className='flex items-center gap-3'>
         <li className='text-black/60'>
             alexa@platzi.com
         </li>
         <li>
           <NavLink
-            to="/my-orders"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/my-orders'
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             My Orders
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/my-account"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/my-account'
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             My Account
           </NavLink>
         </li>
         <li>
           <NavLink
-            to="/sign-in"
-            className={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
+            to='/sign-in'
+            className={({ isActive }) => 
+              isActive ? activeStyle : undefined
+          }>
             Sign In
           </NavLink>
         </li>
         <li className='flex items-center'>
           <ShoppingBagIcon className='size-6 text-black'></ShoppingBagIcon> 
-          <div>{context.count}</div>
+          <div>{context.cartProducts.length}</div>
         </li>
       </ul>
     </nav>
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import Layout from '../../Components/Layout' 
+import Layout from '../../Components/Layout'
 import Card from '../../Components/Card'
 import ProductDetail from '../../Components/ProductDetail'
 import { ShoppingCartContext } from '../../Context'
@@ -8,43 +8,28 @@ function Home() {
   const context = useContext(ShoppingCartContext)
 
   const renderView = () => {
-    if (context.searchByTitle && context.searchByTitle.length > 0) {
-      if (context.filteredItems && context.filteredItems.length > 0) {
-        return (
-          context.filteredItems.map(item => (
-            <Card key={item.id} data={item} />
-          ))
-        )
-      } else {
-        return (
-          <div>We do not have anything</div>
-        )
-      }
+    if (context.filteredItems?.length > 0) {
+      return (
+        context.filteredItems?.map(item => (
+          <Card key={item.id} data={item} />
+        ))
+      )
     } else {
-      if (context.items && context.items.length > 0) {
-        return (
-          context.items.map(item => (
-            <Card key={item.id} data={item} />
-          ))
-        )
-      } else {
-        return (
-          <div>No items available</div>
-        )
-      }
+      return (
+        <div>We do not have anything :(</div>
+      )
     }
   }
-  
 
   return (
     <Layout>
       <div className='flex items-center justify-center relative w-80 mb-4'>
-            <h1 className='font-medium text-xl'>Exclusive Products</h1>
+        <h1 className='font-medium text-xl'>Exclusive Products</h1>
       </div>
-      <input 
-        type='text' 
-        placeholder='Search a product' 
-        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none' 
+      <input
+        type="text"
+        placeholder='Search a product'
+        className='rounded-lg border border-black w-80 p-4 mb-4 focus:outline-none'
         onChange={(event) => context.setSearchByTitle(event.target.value)} />
       <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
         {renderView()}
@@ -53,6 +38,5 @@ function Home() {
     </Layout>
   )
 }
-
 
 export default Home
