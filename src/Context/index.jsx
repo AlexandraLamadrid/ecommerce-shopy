4cfd27fb8bcc0a4fe8155f87043b92b10612c6aa
Alexandra Lamadrid <alexandralamadridcortina@gmail.com>
2024-10-06 01:14:14 -0500

    subiendo cambios en el buscador por filtros

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 export const ShoppingCartContext = createContext()
 
@@ -26,6 +26,27 @@ export const ShoppingCartProvider = ({children}) => {
     // Shopping Cart - Order
     const [order, setOrder] = useState([])
 
+    // Get products
+    const [items, setItems] = useState(null)
+    const [filteredItems, setFilteredItems] = useState(null)
+
+    // Get products by title
+    const [searchByTitle, setSearchByTitle] = useState(null)
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+          .then(response => response.json())
+          .then(data => setItems(data))
+      }, [])
+
+      const filteredItemsByTitle = (items, searchByTitle) => {
+        return items?.filter(item => item.title.toLowerCase().includes(searchByTitle.toLowerCase()))
+      }
+
+      useEffect(() => {
+        if (searchByTitle) setFilteredItems(filteredItemsByTitle(items, searchByTitle))
+      }, [items, searchByTitle])
+
     return (
         <ShoppingCartContext.Provider value={{
             count, 
@@ -41,7 +62,12 @@ export const ShoppingCartProvider = ({children}) => {
             openCheckoutSideMenu,
             closeCheckoutSideMenu,
             order,
-            setOrder
+            setOrder,
+            items,
+            setItems,
+            searchByTitle,
+            setSearchByTitle,
+            filteredItems
         }}>
             {children}
         </ShoppingCartContext.Provider>
